b58dad8bc008b13476dce9419dbcecac0d2554e5
default <isaac.biggs@stu.ocsb.ca>
2025-09-29 13:26:13 +0000

    revised program with proper comments

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+/*
+ * Author: Isaac Biggss
+ *Version: 1.0.0
+ *Date: 2025-9-29
+ *This is the traditional "Hello, World!" program
+ */
+
 package main
 
 import "fmt"
 
 func main() {
-	  fmt.Println("something")
+	//print out the greeting
+	  fmt.Println("Hello, World!")
+
+fmt.Println("\nDone.")
 }
