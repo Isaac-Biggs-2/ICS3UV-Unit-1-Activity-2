54a29e7552c1a17a1f8b6f76528a5cb0878dbc4f
default <isaac.biggs@stu.ocsb.ca>
2025-09-29 13:16:49 +0000

    revised program with proper comments

--- main.ts
+++ main.ts
@@ -1,2 +1,11 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date 2025-09-29
+ * @fileoverview This is the traditional "Hello, World!" program
+ */
 
-console.log("something");
+//print out the greeting
+console.log("Hello World!");
+
+console.log("\nDone.");
